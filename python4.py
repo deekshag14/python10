3b261cb8e02250923684cb10d284136cd89620d9
deekshag14 <124015425+deekshag14@users.noreply.github.com>
2023-02-10 08:00:55 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,21 @@
+#datatype
+#int->store whole number,age=22
+#float->stores number with decimal point.eg.pi=3.14
+#boolean->True,False(1,0)
+#str->group of characters
+#complex->have real part and imaginary eg.3+4j
+
+age=22
+print("Datatype of age:",type(age))
+
+pi=3.14
+print("Datatype of pi:",type(pi))
+
+st_pass=True
+print("Datatype of st_pass:",type(st_pass))
+
+name="Ram"
+print("Datatype of name:",type(name))
+
+c_num=6+6j
+print("Datatype of c_num:",type(c_num))
